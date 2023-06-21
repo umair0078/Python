8156b77315a47f989303bd056f8281ff27f6877a
umair0078 <137155361+umair0078@users.noreply.github.com>
2023-06-21 07:12:01 +0500

    Update for_loop.py

--- DAY 1/for_loop.py
+++ DAY 1/for_loop.py
@@ -1,11 +1,11 @@
-numbers = range(1,  10, -1)
+numbers = range(1,  10)
 
-# for i in numbers:
-#     print(i)
+for i in numbers:
+     print(i)
 
 
-# for x in range(1,21,3):
-#     print(x)
+for x in range(1,21,3):
+     print(x)
 
 
 for x in numbers:
